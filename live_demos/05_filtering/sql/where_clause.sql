e6eaede8a7834c386562bbf65b02544c3478e6f8
fredrikkron <fkron95@gmail.com>
2024-11-18 11:29:34 +0100

    filtering

--- live_demos/05_filtering/sql/where_clause.sql
+++ live_demos/05_filtering/sql/where_clause.sql
@@ -1,1 +1,46 @@
 SELECT * FROM main.data_jobs;
+
+SELECT COUNT(*) FROM main.data_jobs;
+
+-- filtering salary_in_usd lower than 50k
+SELECT * FROM main.data_jobs WHERE salary_in_usd < 50000;
+
+SELECT
+	COUNT(*) AS number_rows_lower_than_50K
+FROM
+	main.data_jobs
+WHERE
+	salary_in_usd < 50000;
+	
+SELECT * FROM main.data_jobs WHERE salary_in_usd < 50000;
+
+-- focus on experience_level
+SELECT
+	DISTINCT experience_level
+FROM
+	main.data_jobs;
+
+SELECT
+	*
+FROM
+	main.data_jobs
+WHERE
+	experience_level = 'EN';
+
+SELECT
+	MEDIAN(salary_in_usd) 
+FROM
+	main.data_jobs
+WHERE
+	experience_level = 'EN';
+
+SELECT
+	MEDIAN(salary_in_usd) 
+FROM
+	main.data_jobs
+WHERE
+	experience_level = 'MI';
+
+
+
+
