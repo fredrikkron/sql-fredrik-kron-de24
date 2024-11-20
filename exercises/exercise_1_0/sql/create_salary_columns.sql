61a3d1b3d06c76a0799e4cda785cd4e3f0b06bab
fredrikkron <fkron95@gmail.com>
2024-11-20 15:06:40 +0100

    created some extra filters

--- exercises/exercise_1_0/sql/create_salary_columns.sql
+++ exercises/exercise_1_0/sql/create_salary_columns.sql
@@ -18,3 +18,43 @@ SELECT
 FROM
 	main.cleaned_salaries;
 
+-- some extra filtering
+SELECT
+	experience_level,
+	employment_type,
+	job_title,
+	yearly_salary_in_SEK,
+	monthly_salary_in_SEK,
+	remote_ratio,
+	company_size,
+	salary_level
+FROM
+	main.cleaned_salaries WHERE experience_level = 'EN' AND job_title = 'Data Engineer'
+	ORDER BY yearly_salary_in_SEK DESC LIMIT 20;
+
+SELECT
+	experience_level,
+	employment_type,
+	job_title,
+	yearly_salary_in_SEK,
+	monthly_salary_in_SEK,
+	remote_ratio,
+	company_size,
+	salary_level
+FROM
+	main.cleaned_salaries
+WHERE
+	company_size = 'Large'
+	AND employment_type = 'Full time'
+	AND yearly_salary_in_SEK < 1000000
+	AND experience_level = 'EN'
+	AND job_title = 'Data Analyst'
+ORDER BY
+	yearly_salary_in_SEK DESC;
+
+
+
+
+
+	
+	
