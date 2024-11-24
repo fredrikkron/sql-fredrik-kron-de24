4f46e376cdaf8027c5aea1f4e438ea55b4a6b4e7
fredrikkron <fkron95@gmail.com>
2024-11-24 18:20:48 +0100

    "added comments"

--- exercises/exercise_2_0/sql/generate_rows.sql
+++ exercises/exercise_2_0/sql/generate_rows.sql
@@ -5,8 +5,10 @@ CREATE TABLE IF NOT EXISTS diceroll (
 
 SELECT * FROM main.diceroll;
 
+-- Resultatet av varje tärningskast 100 ggr
 SELECT FLOOR(RANDOM()* 6 + 1) AS dice FROM generate_series(1,100)
 
+-- Resultatet av hur många gånger man får en 6a i procent
 SELECT
 	AVG(CASE WHEN dice = 6 THEN 1 ELSE 0 END) AS avg_sixes
 FROM
@@ -15,6 +17,6 @@ FROM
 		FLOOR(RANDOM()* 6 + 1) AS dice
 	FROM
 		generate_series(1,
-		1000)) roll_number;
+		100000)) roll_number;
 
 	-- Ju fler gånger man kastar tärningen desto mer konsekvent är tärningens chans att bli ett visst tal. (1/6)
