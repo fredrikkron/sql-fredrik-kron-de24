e4ffa9165ab46f8c543dc299f021c0f9cc0ec8ab
fredrikkron <fkron95@gmail.com>
2024-11-27 10:42:33 +0100

    exercise with strings

---
+++ exercises/exercise_2_1/sql/clean_glossaries.sql
@@ -0,0 +1,27 @@
+CREATE SCHEMA IF NOT EXISTS refined;
+
+CREATE TABLE IF NOT EXISTS refined.sql_glossary AS (SELECT sql_word, description, example FROM staging.sql_glossary);
+
+SELECT * FROM refined.sql_glossary;
+
+SELECT REPLACE(example, 'from', UPPER('from')) AS example FROM refined.sql_glossary;
+
+SELECT
+	REGEXP_REPLACE(TRIM(example), ' ,', ',', 'g') AS example
+FROM
+	refined.sql_glossary;
+
+UPDATE refined.sql_glossary SET example = REPLACE(example, 'TABLE', 'table') WHERE REGEXP_MATCHES(LOWER(example), 'table\b');
+
+	
+UPDATE refined.sql_glossary SET example = REPLACE(example, /b'', UPPER('table'));
+
+
+UPDATE refined.sql_glossary
+SET example = REPLACE(example, 'table', 'TABLE')
+WHERE REGEXP_MATCHES(LOWER(example), '\\btable\\b');
+
+
+SELECT 
+    REPLACE(column_name, 'specificword', UPPER('specificword')) AS transformed_column
+FROM table_name;
---
+++ exercises/exercise_2_1/sql/ingest_glossaries.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE TABLE IF NOT EXISTS staging.sql_glossary AS (
+    SELECT
+        *
+    FROM
+        read_csv_auto ('data/sql_terms.csv')
+);
+
+SELECT * FROM staging.sql_glossary;
