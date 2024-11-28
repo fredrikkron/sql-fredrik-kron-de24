e4c539782182e6890340b45acff16a7c7247cee9
fredrikkron <fkron95@gmail.com>
2024-11-28 15:23:29 +0100

    startup exercise

--- exercises/exercise_2_2/sql/load_sakila.sql
+++ exercises/exercise_2_2/sql/load_sakila.sql
@@ -1,4 +1,4 @@
 INSTALL sqlite;
 LOAD sqlite;
 
-CALL sqlite_attach('Users/kronf/Documents/github/sql-fredrik-kron-de24/exercises/exercise_2_2/data/sqlite-sakila.db');
+CALL sqlite_attach('/Users/kronf/Documents/github/sql-fredrik-kron-de24/exercises/exercise_2_2/data/sqlite-sakila.db');
