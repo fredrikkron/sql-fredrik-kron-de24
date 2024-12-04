d0133d21f4fd656a84fd4a0a3ac7ab59995da4fa
fredrikkron <fkron95@gmail.com>
2024-12-04 13:40:43 +0100

    some exercises

--- exercises/exercise_2_2/sql/eda_sakila.sql
+++ exercises/exercise_2_2/sql/eda_sakila.sql
@@ -24,10 +24,80 @@ SELECT COUNT(*) FROM main.store s; -- 2
 
 -- d)
 SELECT * FROM main.film f ;
-
-SELECT film_id, title, release_year, length AS length_in_minutes, rating FROM main.film f;
+SELECT
+	film_id,
+	title,
+	release_year,
+	length AS length_in_minutes,
+	rating
+FROM
+	main.film f;
+SELECT
+	COUNT(title) AS titles,
+	AVG(length) AS mean_length
+FROM
+	main.film f; -- 1000 movies , avg length of movie is 115 minutes
+SELECT
+	COUNT(*) AS long_movies
+FROM
+	main.film f
+WHERE
+	length > 120; -- 457 movies that are longer than 2 hours
 
 -- e)
+DESC rental;
+SELECT * from main.rental r ;
+SELECT
+	STRFTIME('%H',
+	rental_date) AS time,
+	COUNT(rental_date) AS amount
+FROM
+	main.rental r
+GROUP BY
+	time
+ORDER BY
+	amount DESC;
+
+-- f)
+DESC film;
+SELECT
+	rating,
+	COUNT(rating) AS amount
+FROM
+	main.film f
+GROUP BY
+	rating
+ORDER BY
+	amount DESC;
+
+-- g)
+DESC rental;
+DESC customer;
+
+SELECT
+	c.first_name,
+	c.last_name,
+	COUNT(r.customer_id) AS amount_of_rented_movies
+FROM
+	main.customer c
+LEFT JOIN main.rental r ON
+	c.customer_id = r.customer_id
+GROUP BY
+	c.first_name,
+	c.last_name
+ORDER BY
+	amount_of_rented_movies DESC
+LIMIT 10;
+
+
+
+SELECT * FROM main.rental r WHERE customer_id = 130;
+
+SELECT * FROM main.customer c; 
+
+
+
+
 
 
 
