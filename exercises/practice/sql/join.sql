bf24869fb174f81d5bec77e30f3def77651f8c39
fredrikkron <fkron95@gmail.com>
2024-12-16 11:16:18 +0100

    testing

--- exercises/practice/sql/join.sql
+++ exercises/practice/sql/join.sql
@@ -9,10 +9,27 @@ LEFT JOIN main.products p ON
 	s.product_id = p.product_id; 
 
 SELECT
-	product_name,
-	category,
-	supplier,
-	total_amount
+	p.product_name,
+	p.category,
+	p.supplier,
+	s.total_amount
 FROM
 	main.products p
 INNER JOIN main.sales s ON p.product_id = s.product_id;
+
+
+SELECT
+	p.product_name,
+	p.category,
+	p.supplier,
+	s.total_amount
+FROM
+	main.sales s 
+LEFT JOIN main.products p ON s.product_id = p.product_id;
+
+
+
+
+
+
+
